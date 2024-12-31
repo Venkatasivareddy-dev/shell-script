5f9cc504c878ee40dca0c6b5201373607d4e80b9
Venkata Sivareddy -T (vesivare - WIPRO LIMITED at Cisco) <vesivare@cisco.com>
2024-12-31 21:09:14 +0530

    adding extra1

---
+++ 03varaibles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Siva
+PERSON2=Robert
+
+echo "$PERSON1 :: Hi $PERSON2"
+echo "$PERSON2 :: Hello $PERSON1"
+echo "$PERSON1 :: How are you doing"
+echo "$PERSON2 :: iam doing Good . How are you"
+echo "$PERSON1 :: Iam also good ..Thanks"
