21d74707d52350cfa9a713fac620e6e3d09b9355
Venkata Sivareddy -T (vesivare - WIPRO LIMITED at Cisco) <vesivare@cisco.com>
2024-12-31 22:05:23 +0530

    adding extra1

---
+++ siva.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir cli_backup
+
+chmod 777 cli_backup
+
+cp /etc/NetworkManager/system-connections/ens* /etc/passwd /etc/group /etc/shadow /etc/resolv.conf /etc/hosts /etc/ssh/sshd_config /etc/ssh/ssh_config /etc/scproxyinfo /home/ec2-user/cli_backup
+
+chmod 777 /home/ec2-user/cli_backup/*
+
+zip -r CLI_Backup /home/ec2-user/cli_backup
+
+
